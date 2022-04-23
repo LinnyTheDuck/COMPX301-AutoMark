fdb22010309630d5f4ec19b7a3db9974e8519b81
Linny <github@quackers.slmail.me>
2022-04-23 23:01:57 +1200

    java alphabet check written

--- README.md
+++ README.md
@@ -24,5 +24,5 @@ Will mark a students regex and search assignment in the 12345_12345 folder
 - This is based on the 2021 and 2022 assignment specifications (however it could be adapted for other changes)
 
 ## Todos
-- Sorting: alphabet check, testing
+- Sorting: testing
 - Compression: more accurate cmp feedback, pack/unpack cycle
--- alphabetical.java
+++ alphabetical.java
@@ -1,5 +1,29 @@
+import java.io.*;
+
 public class alphabetical {
     public static void main(String[] args) {
-        
+        try {
+            File sorted = new File("temp/sorted.txt");
+            BufferedReader br = new BufferedReader(new FileReader(sorted));
+            String one = br.readLine();
+            String two = br.readLine();
+            int compare = one.compareTo(two);
+            while(compare <= 0) {  // loop to check order
+                one = two;
+                two = br.readLine();
+                if (two == null)
+                    break;
+                else
+                compare = one.compareTo(two);
+            }  
+            if (compare > 0){ // print out the place where there is an issue for err checking
+                System.out.println(one);
+                System.out.println(two);
+            }
+            else
+                System.out.println("Alphabetical order check sucsessful!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
