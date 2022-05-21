47518281493dcd1e913f229db063195697194e32
Linny <github@quackers.slmail.me>
2022-05-21 21:48:00 +1200

    code cleanup

--- src/checkregex.java
+++ src/checkregex.java
@@ -3,12 +3,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class checkregex {
-    public static List<String> lines = new ArrayList<String>();
+    public static List<String> searchLines = new ArrayList<String>();
     public static File answers = new File("../test_files/answers.txt");
     public static void main(String[] args) {
         try {
-            File quotes = new File("../test_files/" + args[0]); // input file
-            lines = read(quotes); // get full file for checking purposes
+            File fileToSearch = new File("../test_files/" + args[0]); // input file
+            searchLines = read(fileToSearch); // get full file for checking purposes
             int pass = checks(); // run checks for the output files
 
             // this checks the error test
@@ -35,41 +35,41 @@ public class checkregex {
 
     public static int checks(){
         int totalpass = 0, totaltests = 0; // number of tests passed
-        boolean worked = true;
-        File[] farray = filearray();
+        boolean worked = true; // toggles false to allow for counting
+        File[] outputFiles = gatherOutputFiles(); // stores the output files in an array
+
         try {
             BufferedReader br = new BufferedReader(new FileReader(answers));
-            for (File file : farray) { // for each of the output files
+
+            for (File file : outputFiles) { // for each of the output files
                 BufferedReader b = new BufferedReader(new FileReader(file)); // have a file read in
 
-                String[] num = new String[20]; // should be a list
-                num = br.readLine().split(" "); // convert to ints later ig
-                // TODO: store all the indexes inside the list as integers
+                String[] answerLine = new String[1000]; // should be a list, but I dont think there will be more than 1000 matches?
+                answerLine = br.readLine().split(" "); // convert to ints later ig
 
-                for (String s : num) { // compare the ith line to the ith index value in the source
-                    String out = b.readLine();
-                    if(!out.equals(lines.get(Integer.parseInt(s)))){ // if matches keep going
+                for (String s : answerLine) { // compare the ith line to the ith index value in the source
+                    String output = b.readLine();
+                    if(!output.equals(searchLines.get(Integer.parseInt(s)))){ // if matches keep going
                         System.out.println("===== FILE " + totaltests + " CONTENTS ====="); // print out all file contents
                         Runtime rt = Runtime.getRuntime();
-                        Process p = rt.exec("cat ../temp/" + file.getName());
+                        Process p = rt.exec("cat ../temp/" + file.getName()); // run bash command
                         BufferedReader console = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                        String str;
+                        String str; // passes to reader, print reader contents to console
                         while((str = console.readLine()) != null){
                             System.out.println(str);
                         }
 
                         System.out.println("\n===== FILE " + totaltests + " EXPECTED CONTENTS =====");
-                        // print out all expected contents
-                        for (String st : num) {
-                            System.out.println(lines.get(Integer.parseInt(st)));
+                        for (String st : answerLine) { // print out all expected contents
+                            System.out.println(searchLines.get(Integer.parseInt(st)));
                         }
-
 
                         worked = false;
                         break; // break out of looking at the file
                     }
                 }
-                if (worked)
+
+                if (worked) // check to increment counter
                     totalpass++;
                 else
                     worked = true;
@@ -86,23 +86,23 @@ public class checkregex {
         return totalpass;
     }
 
-    public static File[] filearray(){
-        File[] outlines = new File[10]; // shove all the output files in an array idk
+    public static File[] gatherOutputFiles(){
+        File[] outputFiles = new File[10]; // shove all the output files in an array idk
         for(int i = 0; i < 10; i++){
-            outlines[i] = new File("../temp/regout_" + i + ".txt");
+            outputFiles[i] = new File("../temp/regout_" + i + ".txt");
         }
-        return outlines;
+        return outputFiles;
     }
 
     public static List<String> read(File f){
-        List<String> lines_out = new ArrayList<String>(); // use a list instead of array
+        List<String> linesOut = new ArrayList<String>(); // use a list instead of array
         try {
             BufferedReader br = new BufferedReader(new FileReader(f));
             String l = br.readLine();
-            lines_out.add(0, ""); // just a placeholder that's never used
+            linesOut.add(0, ""); // just a placeholder that's never used
             int i = 1;
             while(l != null) { // fill up list
-                lines_out.add(i, l); // add l to be the ith element
+                linesOut.add(i, l); // add l to be the ith element
                 i++;
                 l = br.readLine();
             }
@@ -111,6 +111,6 @@ public class checkregex {
             System.err.println("error with reading the input file");
             e.printStackTrace();
         }
-        return lines_out;
+        return linesOut;
     }
 }
